ae3861db680ddeaf6079c456f8795e6a7bcebfa1
DauerndamProgrammieren <85431885+DauerndamProgrammieren@users.noreply.github.com>
2024-02-26 12:30:21 +0100

    Add files via upload

---
+++ frontend/js/index-reaction.js
@@ -0,0 +1,29 @@
+var i = 0;
+function time() {
+  if (i == 0) {
+    var clocktime = (1/36)
+    i = 1;
+    var elem = document.getElementById("timerbackground");
+    var starttime = 60*60;
+    var width = 100;
+    var round = 0;
+    var id = setInterval(frame, 1000);
+    function frame() {
+      if (width <= 0) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        var butin = document.getElementById("timertext");
+        var butins = document.getElementById("timertextmin");
+        var clocktimer = starttime - round;
+        var clocktimemin = Math.floor(clocktimer / 60);
+        var clocktimesec = clocktimer - (clocktimemin*60);
+        butin.innerHTML = clocktimemin + "min";
+        butins.innerHTML = clocktimesec + "s";
+        width = width - clocktime;
+        elem.style.width = width + "%";
+        round++;
+      }
+    }
+  }
+}
